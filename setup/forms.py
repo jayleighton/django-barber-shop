2c711dd97ddd46f72fd912f271167415f9d3f610
jay.leighton@outlook.com <Bl@ckhawk1>
2024-03-05 09:37:26 +0000

    Add summernote to staff form. Add security to restricted views

--- setup/forms.py
+++ setup/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from home.models import User
+from django_summernote.widgets import SummernoteWidget
 
 class StaffForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'is_staff', 'description', 'image']
+        widgets = {
+            'description': SummernoteWidget(),
+        }
--- setup/views.py
+++ setup/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from home.models import User
 from .forms import StaffForm
 
 # Create your views here.
+
+
 class StaffList(generic.ListView):
     queryset = User.objects.filter(is_staff=True, is_superuser=False)
     template_name = 'setup/staff.html'
 
+
+@login_required
 def select_staff(request):
     queryset = User.objects.filter(is_staff=False, is_superuser=False)
+    print(request.user.is_staff)
     
-    
-    return render(request, 'setup/select_staff.html', {
-        'data': queryset,
-    })
+    if not request.user.is_staff:
+        raise PermissionDenied
+    else:
+        return render(request, 'setup/select_staff.html', {
+            'data': queryset,
+        })
 
-
+@login_required
 def edit_staff(request, user_id):
     queryset = User.objects.all()
     user = get_object_or_404(queryset, id=user_id)
@@ -26,7 +35,10 @@ def edit_staff(request, user_id):
 
     staff_form = StaffForm(instance=user)
 
-    return render(request, 'setup/add_staff.html', {
-        'staff_form': staff_form,
-        'users_id': user.id, 
-    })
+    if not request.user.is_staff:
+        raise PermissionDenied
+    else:
+        return render(request, 'setup/add_staff.html', {
+            'staff_form': staff_form,
+            'users_id': user.id, 
+        })
