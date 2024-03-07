d23b0fe4bd787ba171ba197dafb11edf3f5e824b
jay.leighton@outlook.com <Bl@ckhawk1>
2024-03-07 17:46:19 +0000

    Style booking page

--- booking/views.py
+++ booking/views.py
@@ -9,6 +9,16 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from setup.models import Info, TradingDays
 from home.models import User
+from datetime import datetime, timedelta
+from .models import Booking
 
 def bookings(request):
-    return render(request, 'booking/booking.html')
+    today = datetime(2024,3,5)
+    end_date = today + timedelta(days=20)
+    print(today)
+    # bookings = Booking.objects.filter(booking_start__range=(today.date(), end_date.date()))
+    bookings = Booking.objects.filter(booking_start__gte=today.date())
+    # bookings = Booking.objects.filter(booking_start__startswith=today.date())
+    return render(request, 'booking/booking.html', {
+        'data': bookings,
+    })
