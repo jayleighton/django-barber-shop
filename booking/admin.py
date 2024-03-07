77aa451b67c2e0035fc213f6c3bae740e12ca48c
jay.leighton@outlook.com <Bl@ckhawk1>
2024-03-07 11:41:44 +0000

    Create booking model

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display=('customer_id', 'staff_id', 'service_id', 'booking_start', 'booking_end', 'booking_amount',)
+
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from home.models import User
+from setup.models import Service, Info, TradingDays
 
 # Create your models here.
+class Booking(models.Model):
+    customer_id = models.ForeignKey(User, 
+                                    on_delete=models.CASCADE,
+                                    related_name='user_id')
+    staff_id = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='staff_id')
+    service_id = models.ForeignKey(Service,
+                                   on_delete=models.CASCADE,
+                                   related_name='service_id')
+    booking_start = models.DateTimeField()
+    booking_end = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    booking_amount = models.FloatField()
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'{self.customer_id} {self.staff_id} {self.service_id} {self.booking_start}'
