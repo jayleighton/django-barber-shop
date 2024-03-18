4b39cce54d0ae24dd2520385313885dcdb607bc7
jay.leighton@outlook.com <Bl@ckhawk1>
2024-03-18 17:16:23 +0000

    Update allowed hosts

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "*ui.dev"]
 
 
 # Application definition
